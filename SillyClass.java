fcb1431ca1db40a61d520fad2e84a8d0d24a2910
crispyBrownWork <gbaldovino1@hwemail.com>
2024-09-06 14:41:35 -0700

    Update SillyClass.java
    Added another special message

--- SillyClass.java
+++ SillyClass.java
@@ -1,5 +1,6 @@
 public class SillyClass {
     public static void main(String[] args) {
         System.out.println("This is a silly class!");
+        System.out.println("This is also a goofy class!");
     }
 }
