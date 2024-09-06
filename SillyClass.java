63adde22bb3ab9d9be0219e2b864ee70e2965449
Giovanni Baldovino <gbaldovino1@hwemail.com>
2024-09-06 14:44:03 -0700

    Fixed goofy message
    No longer goofy

--- SillyClass.java
+++ SillyClass.java
@@ -1,6 +1,6 @@
 public class SillyClass {
     public static void main(String[] args) {
         System.out.println("This is a silly class!");
-        System.out.println("This is also a goofy class!");
+        System.out.println("This is NOT a goofy class!");
     }
 }
